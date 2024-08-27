f6a278df45610de633aa313ff77f8a8ad8409d32
huaiyuan <1029848564@qq.com>
2024-08-27 20:52:38 +0800

    fix: revert open href

--- packages/frontend/src/components/BasicWindow.vue
+++ packages/frontend/src/components/BasicWindow.vue
@@ -222,12 +222,7 @@ export default defineComponent({
 				clickNew.value = false;
 			}, 500);
 
-			console.log('onNewonNewonNew', value.value.url);
-			console.log(iframeRef.value);
-			console.log(iframeRef.value.contentWindow);
-			console.log(iframeRef.value.contentWindow.location);
-			console.log(iframeRef.value.contentWindow.location.href);
-			window.open(iframeRef.value.contentWindow.location.href);
+			window.open(value.value.url);
 		};
 
 		let onTop = () => {
