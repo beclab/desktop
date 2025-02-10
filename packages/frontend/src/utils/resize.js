a7bfc1c2c8d8a870dfc29bee575784a9310eedb1
huaiyuan <1029848564@qq.com>
2025-02-10 19:51:32 +0800

    fix: prefetch LaunchPad when page init

--- packages/frontend/src/utils/resize.js
+++ packages/frontend/src/utils/resize.js
@@ -24,7 +24,7 @@ function mobileCheckout() {
 	let flag = navigator.userAgent.match(
 		/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
 	);
-	return flag || window.innerWidth < 800;
+	return flag || window.innerWidth < 720;
 }
 
 function resizeHandler() {
